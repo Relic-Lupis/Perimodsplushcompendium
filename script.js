412371891e04072a2aa7452df4dd5ecaa89b74f2
Relic-Lupis <gofckyourselfbichas@gmail.com>
2026-05-04 13:45:35 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("periimg").addEventListener("click", () =>{
+window.open("https://modrinth.com/mod/createcybernetics");
+window.open("https://modrinth.com/mod/cybernetics-cyberchems");
+})
+document.getElementById("Stoatedandgoated").addEventListener("click", () =>{
+  window.open("numerodos.html")
+})
